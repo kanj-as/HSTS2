b66a03a0d625b242bfb5a6dd63a555c0883be5dc
kanj-as <56594474+kanj-as@users.noreply.github.com>
2020-09-27 10:24:29 +0300

    creating LoginController and file
    Login controller and login fxml file for students

---
+++ src/main/java/org/HSTS_2020/LoginController.java
@@ -0,0 +1,15 @@
+package org.HSTS_2020;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
+import java.io.File;
+import java.net.URL;
+//import java.sql.Connection;
+//import java.io.Statement;
+
+
+public class LoginController {
+}
