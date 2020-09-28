d2c0d0b500e827d19b42b606974f230d7f305a2e
nidaa-hub <60641210+nidaa-hub@users.noreply.github.com>
2020-09-28 16:25:59 +0300

    adding login Teacher and Controller
    adding login Teacher and Controller with fxml file

---
+++ src/main/java/org/HSTS_2020/Client/HSTSgui/LoginTeacherController.java
@@ -0,0 +1,39 @@
+package org.HSTS_2020.Client.HSTSgui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import org.HSTS_2020.App;
+
+import java.io.IOException;
+
+public class LoginTeacherController {
+
+    @FXML
+    private TextField InsertTeacherUserName;
+
+    @FXML
+    private TextField InsertTeacherPassword;
+
+    @FXML
+    private Button TeacherLoginButton;
+
+    @FXML
+    private Button TeacherExitButton;
+
+    @FXML
+    private Label TeacherInvalidLogin;
+
+    @FXML
+    void HandleTeacherExit(ActionEvent event) throws IOException {
+        App.setRoot("main");
+    }
+
+    @FXML
+    void HandleTeacherLogin(ActionEvent event) {
+
+    }
+
+}
