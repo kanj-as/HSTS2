13c321654d609246c1700caf2c210ef8cdc9387b
kanj-as <56594474+kanj-as@users.noreply.github.com>
2020-09-30 10:25:52 +0300

    adding choicebox to Question Controller
    adding choicebox and parameters

--- src/main/java/org/HSTS_2020/Client/HSTSgui/AddQuestionController.java
+++ src/main/java/org/HSTS_2020/Client/HSTSgui/AddQuestionController.java
@@ -1,22 +1,36 @@
 package org.HSTS_2020.Client.HSTSgui;
 
-        import javafx.event.ActionEvent;
-        import javafx.fxml.FXML;
-        import javafx.scene.control.Button;
-        import javafx.scene.control.CheckBox;
-        import javafx.scene.control.TextArea;
-        import javafx.scene.control.TextField;
-        import org.HSTS_2020.App;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import org.HSTS_2020.App;
 
-        import java.io.IOException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AddQuestionController {
+public class AddQuestionController implements Initializable {
+
+    ObservableList list= FXCollections.observableArrayList();
+
+    @FXML
+    private ChoiceBox<String> CourseCBox;
 
     @FXML
     private TextArea AddQuestion;
 
     @FXML
     private TextArea AddAnswar;
+
+    @FXML
+    private ChoiceBox<String> AnswerCbox;
 
     @FXML
     private CheckBox answer1;
@@ -46,6 +60,9 @@ public class AddQuestionController {
     private Button QuestionExit;
 
     @FXML
+    private Button ApplyAnswerChoice;
+
+    @FXML
     void HandleQuestionExit(ActionEvent event) throws IOException {
         App.setRoot("mainTeacher");
     }
@@ -58,6 +75,34 @@ public class AddQuestionController {
     @FXML
     void HandleQuestionPrev(ActionEvent event) {
         //App.setRoot("main");
+    }
+
+    @FXML
+    void HandleApplyAnswer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HandleSaveQuestion(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadAnswer();
+    }
+
+    @FXML
+    private void displayValue(ActionEvent event){
+
+    }
+
+    private void loadAnswer(){
+        list.removeAll(list);
+        String a="Open Answer";
+        String b="Multi Choice";
+        list.addAll(a,b);
+        AnswerCbox.getItems().addAll(list);
     }
 
 }
